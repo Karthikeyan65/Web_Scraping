0451a7557a9038c7c460e932101a87971881eb49
Karthikeyan65 <karthikeyan.k@meyicloud.com>
2024-12-28 18:00:03 +0530

    Additional changes

--- web_parsers/web_parsers/lambda_handler/lambda_handler/data_parsers/Builtrix_demo/utils.py
+++ web_parsers/web_parsers/lambda_handler/lambda_handler/data_parsers/Builtrix_demo/utils.py
@@ -1,6 +1,8 @@
 from playwright.sync_api import sync_playwright
 from urllib.parse import parse_qs, urlparse
 from datetime import datetime, timedelta
+from typing import Any
+
 import requests
 import logging
 
@@ -12,27 +14,49 @@ logger.setLevel(logging.DEBUG)
 start_date = "2024-12-22"
 end_date = "2024-12-22"
 
-url = "https://builtrix.auth.eu-central-1.amazoncognito.com/login?client_id=3vbut4lt2ts1ae7dv8m9kuo4f3&response_type=token&scope=email+openid+phone&redirect_uri=https%3A%2F%2Fexample.com%2Fcallback"
-with sync_playwright() as p:
-    browser = p.chromium.launch(headless=False, slow_mo=70)
-    page = browser.new_page()
-    page.goto(url)
-    print("Page loaded successfully...")
-    page.locator('input#signInFormUsername').nth(1).fill('data@hello-energy.com')
-    page.locator('input#signInFormPassword').nth(1).fill('d&|UN+PX]3X8')
-    button = page.locator('input[type = submit]').nth(1).click()
+def validate(events):
+    try:
+        if "start_date" in events and "end_date" in events:
+            start_date_str = events["start_date"]
+            end_date_str = events["end_date"]
+            format = "%Y-%m-%d"
 
-    page.wait_for_url("**/*", timeout=1200000)
-    final_link = page.url
-    print(final_link)
-    print("Login Successfully...") 
-    browser.close()
+            start_date = datetime.strptime(start_date_str, format)
+            end_date = datetime.strptime(end_date_str, format)
 
-    # Seperate the ID_token in the 
-    fragment = urlparse(final_link).fragment
-    params = parse_qs(fragment)
-    id_token = params.get("id_token", [None])[0]
-    print("ID_TOKEN:", id_token)
+            if start_date >= end_date:
+                return{"Error": "start_date must be less"}
+            print("Validated start_date and the end_date:",start_date_str, end_date_str)
+            return {"start_date": start_date_str, "end_date": end_date_str}
+        else:
+            print("Start date or end date are not provided. So it take the default values will be used...")
+    except ValueError as vee:
+       return {"error": f"Date validation error: {vee}"}
+    except Exception as e:
+       return {"error": f"unexpected error in the date validation:{e}"}
+
+def id(username, paassword):
+    url = "https://builtrix.auth.eu-central-1.amazoncognito.com/login?client_id=3vbut4lt2ts1ae7dv8m9kuo4f3&response_type=token&scope=email+openid+phone&redirect_uri=https%3A%2F%2Fexample.com%2Fcallback"
+    with sync_playwright() as p:
+        browser = p.chromium.launch(headless=False, slow_mo=70)
+        page = browser.new_page()
+        page.goto(url)
+        print("Page loaded successfully...")
+        page.locator('input#signInFormUsername').nth(1).fill('data@hello-energy.com')
+        page.locator('input#signInFormPassword').nth(1).fill('d&|UN+PX]3X8')
+        button = page.locator('input[type = submit]').nth(1).click()
+
+        page.wait_for_url("**/*", timeout=1200000)
+        final_link = page.url
+        print(final_link)
+        print("Login Successfully...") 
+        browser.close()
+
+        # Seperate the ID_token in the 
+        fragment = urlparse(final_link).fragment
+        params = parse_qs(fragment)
+        id_token = params.get("id_token", [None])[0]
+        print("ID_TOKEN:", id_token)
 
     def split_date(start_date: str, end_date: str, max_day: int = 31 ) -> list[tuple[str, str]]:
         format = "%Y-%m-%d"
@@ -51,6 +75,7 @@ with sync_playwright() as p:
             # return available
     # result = split_date('2024-12-01', '2024-12-31')
     # print(result)
+
 
     def consumption_data(auth_token: str, start_date: str, end_date: str, customers: list) -> dict:
         api_url = "https://api.builtrix.tech/get-consumption-data"
@@ -83,4 +108,31 @@ with sync_playwright() as p:
                 consolidated_data[cpe_number] = customer_data
 
         return consolidated_data or {"error": "No data retrieved."}
+    
 
+
+    def extract_data(username: str, password: str, start_date: str,end_date: str, customers: list) -> dict[str]:
+        print("Data Extraction is Started...")
+
+        auth_token = id(username, password)
+
+        extract_data = consumption_data(auth_token, start_date, end_date, customers)
+
+        return extract_data
+    
+
+def data(raw: dict[str, Any],parser: str,group: str,duration: str,timezone: str,) -> list[dict[str, Any]]:
+
+    transformed_date = []
+
+    for entry_cpe in raw.values():
+        cpe_number = entry_cpe.get("cpe_number")
+        unit = entry_cpe.get("unit_Parameter", "kwh")
+        consumption = entry_cpe.get("Consumption_Production", "consumption").lower()
+        name = "active power production" if consumption == "production" else "active power"
+
+        # cpe_series = {
+        #     "end": point.get("timestamp"),
+        # }
+
+        # for point in entry_cpe.get(data, []):
